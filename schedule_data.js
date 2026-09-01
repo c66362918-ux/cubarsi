f8872db7b8aba5e1e9aa93b45183fd8b098c2665
c66362918-ux <c66362918@gmail.com>
2026-09-01 12:09:46 +0800

    Revert La Liga/Serie A/Ligue 1 logos to sponsor-light versions per user feedback; keep Bundesliga/DFB-Pokal fix

--- schedule_data.js
+++ schedule_data.js
@@ -155,10 +155,10 @@ const GENERIC_VIDEOS = {};
 
 const COMPETITION_LOGOS = {
   "Premier League": "https://upload.wikimedia.org/wikipedia/en/f/f2/Premier_League_Logo.svg",
-  "La Liga": "https://upload.wikimedia.org/wikipedia/commons/5/54/LaLiga_EA_Sports_2023_Vertical_Logo.svg",
-  "Serie A": "https://upload.wikimedia.org/wikipedia/en/a/ab/Serie_A_ENILIVE_logo.svg",
+  "La Liga": "https://upload.wikimedia.org/wikipedia/commons/0/0f/LaLiga_logo_2023.svg",
+  "Serie A": "https://upload.wikimedia.org/wikipedia/commons/e/e9/Serie_A_logo_2022.svg",
   "Bundesliga": "https://upload.wikimedia.org/wikipedia/en/d/df/Bundesliga_logo_%282017%29.svg",
-  "Ligue 1": "https://upload.wikimedia.org/wikipedia/commons/7/7b/Logo_Ligue_1_McDonald%27s_2024.svg",
+  "Ligue 1": "https://upload.wikimedia.org/wikipedia/commons/5/5e/Ligue1.svg",
   "UEFA Champions League": "https://upload.wikimedia.org/wikipedia/en/f/f5/UEFA_Champions_League.svg",
   "Carabao Cup": "https://upload.wikimedia.org/wikipedia/en/0/0c/EFL_%28Carabao%29_Cup_Logo.svg",
   "DFB-Pokal": "https://upload.wikimedia.org/wikipedia/en/7/76/DFB-Pokal_Logo_2026.svg",
